ebfb0f99b3a726c951045cf726edc9d25bebbba5
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-13 00:02:21 -0300

    Create Constructor for Bicycle Class

--- classes/Bicycle.dart
+++ classes/Bicycle.dart
@@ -1,7 +1,14 @@
+
 class Bicycle {
   int cadence;
   int speed;
   int gear;
+
+  Bicycle(this.cadence , this.speed , this.gear);
+
 }
 
-void main(List<String> args) {}
+void main(List<String> args) {
+  var bike = Bicycle(13, 23, 3);
+  print(bike);
+}
