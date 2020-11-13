1a9eb2999dc4a4006c03a8eb3ff5445748555edb
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-13 00:11:00 -0300

    Override toString() Method

--- classes/Bicycle.dart
+++ classes/Bicycle.dart
@@ -1,11 +1,13 @@
-
 class Bicycle {
   int cadence;
   int speed;
   int gear;
 
-  Bicycle(this.cadence , this.speed , this.gear);
+  Bicycle(this.cadence, this.speed, this.gear);
 
+  @override
+  String toString() =>
+      'Bicycle cadence : ${cadence} , speed : ${speed} , gear : ${gear}';
 }
 
 void main(List<String> args) {
