2b33387d825e185011fc8d693a00eb117bcd597e
Maximiliano Correa <jmaxicorrea@gmail.com>
2020-11-13 00:41:39 -0300

    Adding method to Bicycle Dart Clas

--- classes/Bicycle.dart
+++ classes/Bicycle.dart
@@ -3,9 +3,9 @@ class Bicycle {
   int _speed;
   int _gear;
 
-  Bicycle(int cadence, int speed, int gear) {
+  Bicycle(int cadence, int gear) {
     _cadence = cadence;
-    _speed = speed;
+    _speed = 0;
     _gear = gear;
   }
 
@@ -13,12 +13,24 @@ class Bicycle {
   int get speed => _speed;
   int get gear => _gear;
 
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
   @override
   String toString() =>
       'Bicycle cadence : ${_cadence} , speed : ${_speed} , gear : ${_gear}';
 }
 
 void main(List<String> args) {
-  var bike = Bicycle(13, 23, 3);
+  var bike = Bicycle(13, 23);
+  print(bike);
+  bike.speedUp(15);
+  print(bike);
+  bike.applyBrake(10);
   print(bike);
 }
